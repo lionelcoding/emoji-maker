b6b259c6eaae4f9698fe3b3b5ab333afddd6763d
lionelcoding <90384236+lionelcoding@users.noreply.github.com>
2025-01-01 18:43:55 +0100

    feat: implement auth middleware with automatic sign-in redirects

---
+++ app/middleware.tsx
@@ -0,0 +1,14 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+    // Routes that can be accessed while signed out
+    publicRoutes: ["/sign-in", "/sign-up"],
+    
+    // Routes that can always be accessed, and have
+    // no authentication information
+    ignoredRoutes: ["/api/public"]
+});
+
+export const config = {
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
